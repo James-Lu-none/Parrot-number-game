da418afa999af7c3d587e844ce8946d117b601ba
Location <jamesabcde277@gmail.com>
2023-04-02 15:27:00 +0800

    value-update-issue

--- lib/pages/guess_page.dart
+++ lib/pages/guess_page.dart
@@ -1,10 +1,121 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+
+
+final GenGame game=GenGame();
+class GenGame
+{
+  int lowerNumber=1;
+  int upperNumber=100;
+  final answer = Random().nextInt(99)+1;
+}
+
 
 class GuessPage extends StatelessWidget {
   const GuessPage({super.key});
+  void updateValue(String value)
+  {
+    int iValue;
+    try
+    {
+      iValue=int.parse(value);
+    }
+    catch(e) {
+      debugPrint(e as String?);
+      return;
+    }
+    debugPrint("num: $iValue");
+    if( iValue<game.answer && iValue>=game.lowerNumber) {
+      game.lowerNumber=iValue;
+      debugPrint("lowerNumber updated=${game.lowerNumber}");
+    }
+    else if( iValue>game.answer && iValue<=game.upperNumber) {
+      game.upperNumber=iValue;
+      debugPrint("upperNumber updated=${game.upperNumber}");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final TextEditingController myController = TextEditingController();
+    myController.addListener((){updateValue(myController.text);});
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:Center(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            const Text(
+              'Guess a number between',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.normal,fontSize: 20),
+            ),
+            Text.rich(
+              TextSpan(
+                children:<TextSpan> [//Q: statement '<TextSpan>' needed?
+                  TextSpan(
+                    text:'${game.lowerNumber}',style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                  ),
+                  const TextSpan(
+                    text:' and ',
+                  ),
+                  TextSpan(
+                    text:'${game.upperNumber}',style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                  )
+                ],
+              ),
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontWeight: FontWeight.normal,fontSize: 20),
+            ),
+
+            SizedBox(
+              height: 100,
+              width:300,
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                  SizedBox(
+                    height: 50,
+                    width:200,
+                    child:
+                    TextField(
+                      controller: myController,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter a num',
+
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child:Stack(
+                        children:[
+                        TextButton(
+                          onPressed: () {
+                            updateValue(myController.text);
+                            },
+                          style: TextButton.styleFrom(
+                            backgroundColor:Colors.blue,
+                            padding: const EdgeInsets.all(16.0),
+                            textStyle: const TextStyle(fontSize: 20),
+                          ),
+                          child: const Icon(Icons.arrow_back,color:Colors.white,size:20),
+                        ),
+                      ]
+                    )
+                  ),
+                ]
+              )
+            ),
+            Text("Answer is ${game.answer}"),
+          ]
+        )
+      )
+    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,47 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:parrot_number/pages/guess_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:Center(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+            const Image(image:AssetImage("assets/images/flutter.png")),
+            const Text('FlutterApp1 - ParrotNumber'),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: 100,
+              height: 50,
+              child:
+                ElevatedButton(
+                  child: const Text(
+                    "start",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                  ),
+                  onPressed: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => GuessPage()),
+                    );
+                  },
+                ),
+            )
+
+          ]
+        )
+      )
+    );
   }
 }
+
